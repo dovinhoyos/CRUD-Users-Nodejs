c644b15d88e023c708db5584a91c9a95554c3a2d
dovinhoyos <dovricarbhoy@gmail.com>
2025-04-09 09:56:06 -0500

    configuracion eslint, manejo del login

---
+++ index.ts
@@ -0,0 +1,19 @@
+
+// const arregloValores = [
+//     {
+//         nombre: 'Juan',
+//         apellido: 'Perez'
+//     },
+//     {
+//         nombre: 'Maria',
+//     }
+// ]
+
+// const metodo = (param: typeof arregloValores) => {
+//     const indexArray = [1, 2]
+
+//     indexArray.forEach(index => {
+//         console.log(index)
+//     }
+// )
+// }
