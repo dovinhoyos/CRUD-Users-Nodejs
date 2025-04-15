f1b8c7cf27db1bd549fe1c751b2070e228fa30fc
dovinhoyos <dovricarbhoy@gmail.com>
2025-04-15 10:46:23 -0500

    agregue una utilidad parseBody

---
+++ utils/parseBody.ts
@@ -0,0 +1,23 @@
+import type { IncomingMessage } from "http";
+import { StringDecoder } from "string_decoder";
+
+export const parseBody = (req: IncomingMessage): Promise<any> => {
+    return new Promise((res, rej) => {
+        const decoder = new StringDecoder("utf8");
+        let buffer = "";
+
+        req.on("data", (chunk) => {
+            buffer += decoder.write(chunk);
+        })
+
+        req.on("end", () => {
+            buffer += decoder.end();
+
+            try {
+                res(JSON.parse(buffer));
+            } catch (err) {
+                rej(err);
+            }
+        });
+    })
+}
