d4c2bd7a9f2c3b7348fcf946b40305d18710a802
dovinhoyos <dovricarbhoy@gmail.com>
2025-04-22 11:31:39 -0500

    Refactor character router for improved readability and added getCharacterById functionality

--- routes/character.ts
+++ routes/character.ts
@@ -1,25 +1,41 @@
-import type { IncomingMessage, ServerResponse } from 'http';
-import { authenticateToken, type AuthenticatedRequest } from '../middleware/authentication';
-import { getAllCharacters } from '../models';
+import type { IncomingMessage, ServerResponse } from "http";
+import {
+  authenticateToken,
+  type AuthenticatedRequest,
+} from "../middleware/authentication";
+import { getAllCharacters, getCharacterById } from "../models";
 
 export const characterRouter = async (
-    req: IncomingMessage,
-    res: ServerResponse
+  req: IncomingMessage,
+  res: ServerResponse
 ) => {
-    const { method, url } = req;
-    
-    if(!await authenticateToken(req as AuthenticatedRequest, res)){
-        res.statusCode = 401;
-        res.end(JSON.stringify({ message: 'Unauthorized' }));
-        return;
-    };
+  const { method, url } = req;
 
-    if(url === '/characters' && method === 'GET'){
-        const characters = getAllCharacters();
-        res.statusCode = 200;
-        res.end(JSON.stringify(characters));
-        return;
+  if (!(await authenticateToken(req as AuthenticatedRequest, res))) {
+    res.statusCode = 401;
+    res.end(JSON.stringify({ message: "Unauthorized" }));
+    return;
+  }
+
+  if (url === "/characters" && method === "GET") {
+    const characters = getAllCharacters();
+    res.statusCode = 200;
+    res.end(JSON.stringify(characters));
+    return;
+  }
+
+  if (url === "/characters/" && method === "GET") {
+    const id = parseInt(url.split("/").pop() as string, 10);
+    const character = getCharacterById(id);
+
+    if (!character) {
+      res.statusCode = 404;
+      res.end(JSON.stringify({ message: "Character not found" }));
+      return;
     }
-    
 
-}
+    res.statusCode = 200;
+    res.end(JSON.stringify(character));
+    return;
+  }
+};
